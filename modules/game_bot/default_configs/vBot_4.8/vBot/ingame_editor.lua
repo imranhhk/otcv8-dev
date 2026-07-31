f39ed01f259e2061267d7a8c4b7d866a535fa8cb
Imranhhk <imranhhk@yahoo.com>
2026-07-30 20:55:03 -0700

    Support loading outsite macro

--- modules/game_bot/default_configs/vBot_4.8/vBot/ingame_editor.lua
+++ modules/game_bot/default_configs/vBot_4.8/vBot/ingame_editor.lua
@@ -1,23 +1,28 @@
 setDefaultTab("Tools")
 -- allows to test/edit bot lua scripts ingame, you can have multiple scripts like this, just change storage.ingame_lua
 UI.Button("Ingame script editor", function(newText)
-    UI.MultilineEditorWindow(storage.ingame_hotkeys or "", {title="Hotkeys editor", description="You can add your custom scrupts here"}, function(text)
+    UI.MultilineEditorWindow(storage.ingame_hotkeys or "", {title="Hotkeys editor", description="You can add your custom scripts here"}, function(text)
       storage.ingame_hotkeys = text
       reload()
     end)
   end)
-  
+
   UI.Separator()
-  
-  for _, scripts in pairs({storage.ingame_hotkeys}) do
+
+  for _, scripts in pairs({storage.text}) do
     if type(scripts) == "string" and scripts:len() > 3 then
+      error("Scripts: " .. scripts:len())
       local status, result = pcall(function()
-        assert(load(scripts, "ingame_editor"))()
+        if _VERSION == "Lua 5.1" and type(jit) ~= "table" then
+          return assert(loadstring(scripts))()
+        else        
+          assert(load(scripts, "ingame_editor"))()
+        end
       end)
-      if not status then 
+      if not status then
         error("Ingame edior error:\n" .. result)
       end
     end
   end
-  
+
   UI.Separator()
